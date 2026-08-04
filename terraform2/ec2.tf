de0a51803cbddcc957cd46e37be13c4e9735e879
mohamadarifin97 <itachisooltan@gmail.com>
2026-08-04 08:11:17 +0800

    add ec2.tf

---
+++ terraform2/ec2.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+data "aws_iam_instance_profile" "my_ssm_profile" {
+  name = "EC2-SSM-Role"
+}
+
+resource "aws_instance" "my_server_1" {
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  iam_instance_profile   = data.aws_iam_instance_profile.my_ssm_profile.name
+
+  tags = {
+    Name = "tf-server-1"
+  }
+}
